403a3d9c7652372a2dc364882074a440738941ee
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-10 20:28:06 +0900

    @ConfigurationProperties 개선 2부

--- src/main/java/me/whiteship/demospringboot22/ChickenProperties.java
+++ src/main/java/me/whiteship/demospringboot22/ChickenProperties.java
@@ -1,25 +1,24 @@
 package me.whiteship.demospringboot22;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "chicken")
 public class ChickenProperties {
     private String name;
     private int count;
 
+    public ChickenProperties(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCount() {
         return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
     }
 }
